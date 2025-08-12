a0bb535463c160e187d778f20800a1660167d2be
Fernando Perry <fernandoperry1234@gmail.com>
2025-08-12 14:37:07 +0700

    small change on main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include "stateman.h"
 
+#define WINAME "imgview"
 #define WINSIZE_X 1280
 #define WINSIZE_Y 720
 #define WINFPS    60
@@ -12,13 +13,15 @@ int main(int argc, char **argv) {
     ProgramOpt opt(argc, argv, 2);
     if (opt.mError > 0) return -1;
 
-    Window w(WINSIZE_X, WINSIZE_Y, WINFPS, "imgview");
-    std::optional<std::string> fail = w.initWindow();
+    Window w(WINSIZE_X, WINSIZE_Y, WINFPS, WINAME);
+    auto fail = w.initWindow();
     if (fail.has_value()) std::cerr << fail.value();
 
     StateManager sm = {};
 
     int idx = sm.makeNewState(&w, opt.mInputFile[0].c_str());
+    if (idx < 0) return -1;
+
     if (!sm.activeState(idx)) return -1;
     if (!w.startWindowLoops(&sm)) return -1;
 
